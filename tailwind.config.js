633128f45d29c31d8b0de804defc33734087a51b
Kieran <kiekelly1976@gmail.com>
2021-12-07 16:15:25 +0000

    tailwind changes to style the blog

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,22 @@ module.exports = {
     },
   },
   theme: {
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
+      //serif: ['Merriweather', 'serif'],
+    },
+    fontWeight: {
+      bold: 700,
+      semibold: 600,
+      medium: 500,
+    },
     extend: {
+      colors: {
+        'peg-grey': '#EFF0F6',
+        'peg-grey-light': '#F7F7FC',
+        'peg-blue': '#0576F0',
+        'peg-blue-dark': '#005BD4',
+      },
       typography: {
         kieran: {
           css: {
